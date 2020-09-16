5c4406d3008cb7e6db8e0ec40381ef7db16418dc
fluturecode <fluturecode@gmail.com>
2020-09-16 14:57:35 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDBG3sAyArUAa--f0aoahzqtSRvktLT4DM",
+	authDomain: "e-commerce-site-1888f.firebaseapp.com",
+	databaseURL: "https://e-commerce-site-1888f.firebaseio.com",
+	projectId: "e-commerce-site-1888f",
+	storageBucket: "e-commerce-site-1888f.appspot.com",
+	messagingSenderId: "502135017884",
+	appId: "1:502135017884:web:37e320a0876a4da614b859",
+	measurementId: "G-5FLLJFP7HR",
+};
