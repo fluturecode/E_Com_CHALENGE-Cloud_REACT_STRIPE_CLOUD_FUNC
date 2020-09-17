57a4318c93269172cd3e6d56d07aa8f93a1811c2
fluturecode <fluturecode@gmail.com>
2020-09-16 20:27:26 -0400

    added remove from basket feature

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
-// import { useStateValue } from "./StateProvider";
-// import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "../context/stateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
-	// const [{ basket, user }, dispatch] = useStateValue();
+	const [{ basket }] = useStateValue();
 
 	return (
 		<div className="checkout">
@@ -13,14 +13,14 @@ function Checkout() {
 				<img
 					className="checkout__ad"
 					src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-					alt=""
+					alt="amazon checkout"
 				/>
 
 				<div>
 					{/* <h3>Hello, {user?.email}</h3> */}
-					<h2 className="checkout__title">Your shopping Basket</h2>
+					<h2 className="checkout__title">Your Shopping Basket</h2>
 
-					{/* {basket.map((item) => (
+					{basket.map((item) => (
 						<CheckoutProduct
 							id={item.id}
 							title={item.title}
@@ -28,7 +28,7 @@ function Checkout() {
 							price={item.price}
 							rating={item.rating}
 						/>
-					))} */}
+					))}
 				</div>
 			</div>
 
