c281dd7486294502adecc1f89e4bdc73713f5788
fluturecode <fluturecode@gmail.com>
2020-09-16 20:41:09 -0400

    added Login component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout.js";
+import Login from "./components/Login";
 
 function App() {
 	return (
 		<Router>
 			<div className="app">
-				<Header />
 				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
 					<Route path="/checkout">
+						<Header />
 						<Checkout />
 					</Route>
 					<Route path="/">
+						<Header />
 						<Home />
 					</Route>
 				</Switch>
--- src/components/Header.js
+++ src/components/Header.js
@@ -25,10 +25,12 @@ function Header() {
 			</div>
 
 			<div className="header__nav">
-				<div className="header__option">
-					<span className="header__optionLineOne"></span>
-					<span className="header__optionLineTwo"></span>
-				</div>
+				<Link to="login">
+					<div className="header__option">
+						<span className="header__optionLineOne">Hello Guest</span>
+						<span className="header__optionLineTwo">Sign In</span>
+					</div>
+				</Link>
 
 				<div className="header__option">
 					<span className="header__optionLineOne">Returns</span>
