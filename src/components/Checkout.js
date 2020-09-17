43924f43a6afd0d0bb23b1125229881354a4ce53
fluturecode <fluturecode@gmail.com>
2020-09-16 21:43:37 -0400

    added Build directory & deployed frontend to Firebase

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from "../context/stateProvider";
 import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
-	const [{ basket }] = useStateValue();
+	const [{ basket, user }] = useStateValue();
 
 	return (
 		<div className="checkout">
@@ -17,7 +17,7 @@ function Checkout() {
 				/>
 
 				<div>
-					{/* <h3>Hello, {user?.email}</h3> */}
+					<h3>Hello, {user?.email}</h3>
 					<h2 className="checkout__title">Your Shopping Basket</h2>
 
 					{basket.map((item) => (
