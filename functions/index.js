baf497d3fa62d6f72fbf317c0a0b13b285ae6ebe
Elizabeth Eidelson <fluturecode@gmail.com>
2020-09-18 14:01:27 -0400

    deployed server

--- functions/index.js
+++ functions/index.js
@@ -16,6 +16,20 @@ app.use(express.json());
 
 // API Routes
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+// Grab the endpoint created in payments.js
+app.post("/paments/create", async (req, res) => {
+	const total = req.query.total;
+
+	console.log("Payment Request Received for this amount >>>", total);
+
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: total,
+		currency: "usd",
+	});
+	res.status(201).send({
+		clientSecret: paymentIntent.client_secret,
+	});
+});
 
 // Listen Command
 exports.api = functions.https.onRequest(app);
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "", // Cloud function URL
+	baseURL: "http://localhost:5001/e-commerce-site-1888f/us-central1/api", // Cloud function URL
 });
 
 export default instance;
